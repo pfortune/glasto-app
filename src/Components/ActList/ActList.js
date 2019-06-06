112a78db999d4386cf4d4cb39a7ba08ffda41546
Peter Fortune <pfortune@gmail.com>
2019-06-06 10:04:12 +0100

    fixed actlist component

--- src/Components/ActList/ActList.js
+++ src/Components/ActList/ActList.js
@@ -6,30 +6,37 @@ class ActList extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       data: this.props.data,
-      name: this.props.name 
+      name: this.props.name
     };
-
   }
 
-  handleTitleClick(){
+  handleTitleClick() {
     alert("You clicked me");
   }
 
-  render(){
+  render() {
     let acts = [];
-    for(let i=3500; i <= this.state.data.length; i++){
-      console.log(this.state.data[i]);
-      //console.log(act.act);
-      //acts.push(<Act name={act.act} stage={act.stage} date={act.date} time={act.start}></Act>);
+    for (let i = 3500; i < this.state.data.length; i++) {
+      let act = this.state.data[i];
+
+      acts.push(
+        <Act
+          name={act.act}
+          stage={act.stage}
+          date={act.date}
+          time={act.start}
+          key={i}
+        />
+      );
     }
 
     return (
       <div>
         <h1 onClick={this.handleTitleClick}>Act List</h1>
         <SearchParams />
-        <Act name="Snow Patrol" stage="Pyramid" date="29/06/2019" time="5pm" />
+        {acts}
       </div>
     );
   }
